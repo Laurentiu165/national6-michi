51aee4aa9906a5c838e7bc418f1d30b53687314a
Laurentiu <michi.laurentiu@yahoo.com>
2021-03-10 12:32:51 +0200

    Clean start again

--- getData.js
+++ getData.js
@@ -2,11 +2,12 @@ console.log("CRUD Operation Homework");
 
 const articleListHtml = document.querySelector(".article-list");
 
-document.getElementById("get-data").addEventListener("click", function(){
-  fetch("https://simple-json-server-scit.herokuapp.com/comments").then(handleFetchResponse).then(useJSONResponse);
-  fetch("https://simple-json-server-scit.herokuapp.com/posts").then(handleFetchResponse).then(useJSONResponse);
-  
-});
+document.getElementById("get-data").addEventListener("click", getData);
+
+function getData(){
+    fetch("https://simple-json-server-scit.herokuapp.com/posts").then(handleFetchResponse).then(useJSONResponse);
+    
+}
 function handleFetchResponse(response){
   console.log("response",response);
   return response.json();
@@ -31,17 +32,11 @@ function renderArticle(articleData){
   const article = document.createElement("div");
   const articleTitle = document.createElement("h3");
   const articleContent = document.createElement("p");
-  const articleComments = document.createElement("div")
-  const articleUser = document.createElement("h4");
-  
-  articleComments.appendChild(articleUser);
-  article.appendChild(articleComments);
+
   article.appendChild(articleTitle);
   article.appendChild(articleContent);
-  
   articleListHtml.appendChild(article);
 
-  articleUser.innerText = articleData.username;
   articleTitle.innerText = articleData.title;
   articleContent.innerText = articleData.content;
 };
