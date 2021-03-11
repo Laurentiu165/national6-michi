888ac151255d083f92f3e081d1d2adf47252414c
Laurentiu <michi.laurentiu@yahoo.com>
2021-03-11 12:32:16 +0200

    finished homework

--- getData.js
+++ getData.js
@@ -42,10 +42,44 @@ function renderArticle(articleData){
 
   fetch("https://simple-json-server-scit.herokuapp.com/comments?postId=" + articleData.id).then(handleFetchResponse).then(useJSONResponse);
 
+  function handleFetchResponse(response){
+    console.log("response", response);
+    return response.json()
+  };
+
+  function useJSONResponse(json){
+    console.log(json);
+    renderComments(json);
+  };
+
   function renderComments(articleCommentsList){
     for(articleComments of articleCommentsList) {
      console.log(articleComments); 
      renderComment(articleComments);
     }
   }
+  
+  function renderComment(articleComments){
+    const articleCommentHtml = document.createElement("div");
+    const allComments = document.createElement("div");
+    const commentUser = document.createElement("h4");
+    const commentContent = document.createElement("p");
+
+
+    articleCommentHtml.appendChild(allComments);
+    allComments.appendChild(commentUser);
+    allComments.appendChild(commentContent);
+    article.appendChild(articleCommentHtml);
+
+    articleCommentHtml.classList.add("post-comments");
+    allComments.classList.add("comments");
+    allComments.style.paddingLeft = "20px";
+
+    commentUser.innerText = articleComments.username;
+    commentContent.innerText = articleComments.content;
+
+
+  }
+
+
 };
