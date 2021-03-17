b7ed2f18454c61ab0166e41c9821215825ce3f17
Laurentiu <michi.laurentiu@yahoo.com>
2021-03-17 15:07:51 +0200

    not done

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 console.log("To-Do-List Homework");
+
+const toDoInput = document.querySelector(".task-input").value;
+
+const addTaskHtml = document.querySelector(".add-task");
+
+document.getElementById("add-task-img").addEventListener("click", () =>{
+  console.log("I click on add")
+  if(toDoInput){
+    const payload = {
+      id: "mLaurentiu",
+      todo: [
+        {
+          checked: false,
+          item: toDoInput
+        }
+      ]
+    }
+    fetch("https://simple-json-server-scit.herokuapp.com/todo",{
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(payload)
+    })
+    console.log("payload", payload);
+  }
+})
