f01a11cb75d4302ff3c77f5c7731e5e4b4bfacf5
Laurentiu <michi.laurentiu@yahoo.com>
2021-03-18 14:32:58 +0200

    some errors but on the right track

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 console.log("To-Do-List Homework");
 
-const toDoInput = document.querySelector(".task-input").value;
 
 const addTaskHtml = document.querySelector(".add-task");
 
 document.getElementById("add-task-img").addEventListener("click", () =>{
+  const toDoInput = document.querySelector(".task-input").value;
   console.log("I click on add")
   if(toDoInput){
     const payload = {
@@ -16,13 +16,39 @@ document.getElementById("add-task-img").addEventListener("click", () =>{
         }
       ]
     }
+
+    console.log("Payload", payload);
+    console.log("Payload text:", JSON.stringify(payload));
+
     fetch("https://simple-json-server-scit.herokuapp.com/todo",{
       method: "POST",
       headers: {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify(payload)
-    })
-    console.log("payload", payload);
+    }).then(getData);
+  }
+  function getData(){
+    fetch(`https://simple-json-server-scit.herokuapp.com/todo/mLaurentiu`).then(handleFetchResponse).then(useJSONResponse)
+
+    
+  }
+
+  function handleFetchResponse(response) {
+    console.log("response", response);
+    return response.json();
+  }
+
+  function useJSONResponse(json) {
+    console.log(json);
+    renderArticles(json)
+  }
+
+  function renderArticles(articleList) {
+    addTaskHtml.innerHTML = "";
+    for (const articleData of articleList) {
+        console.log(articleData);
+        
+    }
   }
 })
