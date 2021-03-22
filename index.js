0f57c2bd6b78cf254f1cf446d495df109ad82be8
Laurentiu <michi.laurentiu@yahoo.com>
2021-03-22 15:06:15 +0200

    finished homework

--- index.js
+++ index.js
@@ -1,54 +1,157 @@
-console.log("To-Do-List Homework");
+console.log('to-do-app-homework-mLaurentiu')
+
+const taskListHtml = document.querySelector(".add-task");
+
+document.getElementById("add-task-img").addEventListener("click", getData);
+
+function postData(addTask) {
+  const payload = {
+    id: "mLaurentiu",
+    todo: [
+      {
+        checked: false,
+        item: addTask
+      }
+    ],
+  }
+
+  return fetch('https://simple-json-server-scit.herokuapp.com/todo', {
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(payload)
+  });
+
+}
+
+function addingData(addedTask, get) {
+  const newTask = {
+    checked: false,
+     item: addedTask
+   };
+  get.todo.push(newTask);
+  return fetch("https://simple-json-server-scit.herokuapp.com/todo/mLaurentiu", {
+    method: "PUT",
+     headers: {
+      'Content-Type': 'application/json'
+     },
+    body: JSON.stringify(get)
+  });
+}
+function getData() {
+  const addTask = document.getElementById("task-input").value;
+  fetch("https://simple-json-server-scit.herokuapp.com/todo/mLaurentiu").then(r => r.json()).then(get => {
+      if (get.todo) {
+        return addingData(addTask, get);
+      } else {
+        return postData(addTask);
+      }
+    })
+    .then(() => renderToDoList());
+}
 
 
-const addTaskHtml = document.querySelector(".add-task");
 
-document.getElementById("add-task-img").addEventListener("click", () =>{
-  const toDoInput = document.querySelector(".task-input").value;
-  console.log("I click on add")
-  if(toDoInput){
-    const payload = {
-      id: "mLaurentiu",
-      todo: [
-        {
-          checked: false,
-          item: toDoInput
-        }
-      ]
+function renderToDoList() {
+  fetch("https://simple-json-server-scit.herokuapp.com/todo/mLaurentiu")
+    .then(r => r.json())
+    .then((get) => {
+      console.log(get);
+      taskListHtml.innerHTML = "";
+      for (const element of get.todo) {
+         renderToDoTask(element, get);
+      }
+      
+    })
+
+}
+
+function renderToDoTask(element, data) {
+
+  const taskContent = document.createElement("div");
+  taskContent.classList.add("all-task-content");
+  const checkedTask = document.createElement("input");
+  checkedTask.setAttribute("type", "checkbox");
+
+  const taskText = document.createElement("h3");
+  
+  const deleteTask = document.createElement("img");
+  deleteTask.classList.add("delete-task-img")
+  deleteTask.setAttribute("src", "bin.svg")
+  
+
+  taskContent.appendChild(checkedTask);
+  taskContent.appendChild(taskText);
+  taskContent.appendChild(deleteTask);
+  taskListHtml.appendChild(taskContent);
+
+  taskText.innerText = element.item;
+
+  deleteTask.addEventListener("click", function () {
+    taskContent.remove();
+    deleteTasks(element.item, data);
+
+  });
+  checkedTask.addEventListener("click", function() {
+    if (checkedTask.checked === true) {
+      element.checked = true;
+      return fetch("https://simple-json-server-scit.herokuapp.com/todo/mLaurentiu", {
+        method: "PUT",
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      });
+    } else if (checkedTask.checked === false){
+      element.checked = false;
+      return fetch("https://simple-json-server-scit.herokuapp.com/todo/mLaurentiu", {
+        method: "PUT",
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      });
     }
+  })
+ }
 
-    console.log("Payload", payload);
-    console.log("Payload text:", JSON.stringify(payload));
+function deleteTasks(item, data) {
 
-    fetch("https://simple-json-server-scit.herokuapp.com/todo",{
-      method: "POST",
+  const index = data.todo.findIndex((task) => {
+    return task.item === item
+  });
+  data.todo.splice(index, 1);
+
+  fetch(
+    "https://simple-json-server-scit.herokuapp.com/todo/mLaurentiu",
+    {
+      method: "PUT",
       headers: {
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify(payload)
-    }).then(getData);
-  }
-  function getData(){
-    fetch(`https://simple-json-server-scit.herokuapp.com/todo/mLaurentiu`).then(handleFetchResponse).then(useJSONResponse)
-
-    
-  }
-
-  function handleFetchResponse(response) {
-    console.log("response", response);
-    return response.json();
-  }
-
-  function useJSONResponse(json) {
-    console.log(json);
-    renderArticles(json)
-  }
-
-  function renderArticles(articleList) {
-    addTaskHtml.innerHTML = "";
-    for (const articleData of articleList) {
-        console.log(articleData);
-        
+      body: JSON.stringify(data)
     }
-  }
-})
+  )
+}
+
+document.addEventListener("DOMContentLoaded", onRefresh);
+
+
+function fetchData() {
+  return fetch("https://simple-json-server-scit.herokuapp.com/todo/mLaurentiu")
+  
+  .then((r) => r.json())
+  .then((data) => data);
+}
+
+function renderContent(data) {
+  
+  data.todo.forEach(data => {
+    renderToDoList(data);
+  });
+}
+function onRefresh() {
+  fetchData()
+    .then(data => renderContent(data));
+}
