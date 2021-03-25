a89b4f37c972ef2e0695a8a28557eeafa1038a5f
Laurentiu <michi.laurentiu@yahoo.com>
2021-03-25 21:03:10 +0200

    game finished

--- game.js
+++ game.js
@@ -23,6 +23,9 @@ class GameObject{
     this.ref.style.transform = `translate(${this.x}px, ${this.y}px)`;
 
   }
+  removeRef(){
+    this.ref.remove();
+  }
 }
 
 
@@ -33,11 +36,14 @@ class Player extends GameObject{
     this.move(50, 225);
   }
   moveUp(){
-    this.move(this.x, this.y - 25);
+    if(this.y -25 >= 0)
+      this.move(this.x, this.y - 25);
   }
   moveDown(){
-    this.move(this.x, this.y + 25);
+    if(this.y + 25 <= 500 - this.height)
+      this.move(this.x, this.y + 25);
   }
+  
 }
 
 let keyUpPress = false;
@@ -59,6 +65,7 @@ document.addEventListener("keyup", (event) => {
     keyDownPress= false;
   }
 });
+
 class Obstacle extends GameObject{
   constructor(){
     super();
@@ -71,12 +78,65 @@ class Obstacle extends GameObject{
   
 }
 
-const player = new Player();
-const obstacle = new Obstacle()
+class ObstacleFactory{
+  constructor(){
+    this.obstacles = [];
+  }
+  createObstacle(){
+    const obstacle = new Obstacle();
+    obstacle.move(1060, Math.floor(Math.random()* 450));
+    this.obstacles.push(obstacle);
+  }
+  destroyObstacle(){
+    this.obstacles = this.obstacles.filter((obstacle) =>{
+      if(obstacle.x < -50){
+        obstacle.removeRef();
+        return false;
+      }
+      return true;
+    })
+  }
 
-setInterval(() =>{
-// console.log(keyUpPress);
-if(keyUpPress) player.moveUp();
-if(keyDownPress) player.moveDown();
-obstacle.moveLeft();
-},250)
+  
+  moveObstacles(){
+    for(const obstacle of this.obstacles){
+      obstacle.moveLeft();
+    }
+  }
+}
+
+function collisionDetection(player, obstacles){
+  for(const obstacle of obstacles){
+    if (
+      (player.x <= obstacle.x &&
+      obstacle.x <= player.x + player.width &&
+      player.y <= obstacle.y &&
+      obstacle.y <= player.y + player.height) ||
+      (player.x <= obstacle.x + obstacle.width &&
+      obstacle.x + obstacle.width <= player.x + player.width &&
+      player.y <= obstacle.y + obstacle.height &&
+      obstacle.y + obstacle.height <= player.y + player.height)
+    ) return true;
+  }
+  return false;
+}
+
+const player = new Player();
+const obstacleFactory = new ObstacleFactory();
+
+let count = 0;
+
+let gameLoop = setInterval(() =>{
+  if(keyUpPress) player.moveUp();
+  if(keyDownPress) player.moveDown();
+  if(count % 20 === 0) obstacleFactory.createObstacle();
+  obstacleFactory.moveObstacles();
+  if (collisionDetection(player, obstacleFactory.obstacles)){
+     clearInterval(gameLoop);
+    alert("You hit an obstacle");
+    window.location = "/";
+  } 
+
+  obstacleFactory.destroyObstacle();
+  count++;
+},50)
