37f7fef1b34cf06e9b539845f07045d715505d01
Laurentiu <michi.laurentiu@yahoo.com>
2021-04-01 20:52:18 +0300

    local storage done

--- index.js
+++ index.js
@@ -1,31 +1,13 @@
 console.log("JavaScript - Cookies & Local Storage");
 
-console.log(document.cookie);
-
-if(!document.cookie){
+if (!localStorage.getItem("name") || !localStorage.getItem("password")){
   window.location = "/login.html"
-}
-
-if (document.cookie){
-  const cookiesElements = document.cookie.split("; ");
-  console.log(cookiesElements);
-  const cookieObj = {}
-  for(const element of cookiesElements){
-    const [key, value] = element.split("=");
-    cookieObj[key] = value;
-    console.log(key, value);
-
   }
-  console.log(cookieObj);
-  if(!cookieObj.name || !cookieObj.password){
-    window.location = "/login.html"
-
-  }
-}
 
 document.getElementById("log-out").addEventListener("click", () => {
-  document.cookie = "password=123; Expires =Thu, 31 Oct 1990 00:00:00 gtm";
-  document.cookie = "name=123; Expires =Thu, 31 Oct 1990 00:00:00 gtm";
+  localStorage.removeItem("name");
+  localStorage.removeItem("password");
+
   window.location = "/";
 })
 
