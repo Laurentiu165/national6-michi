e257efe3566a38c596e7fcf36d2ca934045314b8
Laurentiu <michi.laurentiu@yahoo.com>
2021-04-07 12:27:43 +0300

    error...

--- index.js
+++ index.js
@@ -9,3 +9,28 @@ document.getElementById("logout").addEventListener("click", () => {
   localStorage.removeItem("password");
   window.location = "/";
 });
+
+let breeds = document.getElementById("breeds");
+function getData() {
+  fetch("https://dog.ceo/api/breeds/list/all")
+    .then(handleFetchResponse)
+    .then(useJSONResponse);
+}
+
+function handleFetchResponse(response) {
+  console.log("response", response);
+  return response.json();
+}
+
+function useJSONResponse(json) {
+  console.log(json);
+  renderArticles(json);
+}
+
+function renderArticles(articleList) {
+  for (const articleData of articleList) {
+    console.log(articleData);
+    renderArticle(articleData);
+  }
+}
+getData();
