7ed9f8c8416ac16f2d3c6ac3c40a82d0ccaa5d10
Laurentiu <michi.laurentiu@yahoo.com>
2021-04-10 20:06:22 +0300

    almost done

--- index.js
+++ index.js
@@ -41,10 +41,39 @@ function renderBreed(breedName) {
   breedParagraph.addEventListener("click", () => {
     console.log("i clicked on: ", breedParagraph.innerText);
     fetch(`https://dog.ceo/api/breed/${breedParagraph.innerText}/images`)
-      .then((r) => r.json)
-      .then(useJSONResponseImage);
+      .then((r) => r.json())
+      .then(renderBreedImages);
   });
-  function useJSONResponseImage(json) {}
+}
+
+let breedImage = document.getElementById("breed-image");
+let pageNumber = document.getElementById("page-number");
+let breedImageArray = [];
+let i = 0;
+
+function renderBreedImages(image) {
+  breedImageArray = image.message;
+  console.log(breedImageArray);
+  breedImage.src = breedImageArray[0];
+}
+document.getElementById("forward").addEventListener("click", forward);
+
+function forward() {
+  if (i < breedImageArray.length) {
+    i++;
+    breedImage.src = breedImageArray[i];
+    pageNumber.innerText = i + 1;
+  }
+}
+
+document.getElementById("backward").addEventListener("click", backward);
+
+function backward() {
+  if (pageNumber.innerText > 1) {
+    breedImage.src = breedImageArray[i - 1];
+    pageNumber.innerText = i;
+    i--;
+  }
 }
 
 getData();
