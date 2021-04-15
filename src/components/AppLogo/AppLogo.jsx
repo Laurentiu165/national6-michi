9248252b6a2d257e1121c73fcee009adb1ca1d61
Laurentiu <michi.laurentiu@yahoo.com>
2021-04-15 21:01:54 +0300

    starting to-do-app in react

---
+++ src/components/AppLogo/AppLogo.jsx
@@ -0,0 +1,6 @@
+import toDoSvg from "./to-do-list.svg"
+import "./AppLogo.css"
+
+export function AppLogo(){
+  return <img src={toDoSvg} alt="app-logo" className="app-logo"/>
+}
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,8 @@
+import { AppLogo } from "../AppLogo/AppLogo"
+import "./Header.css"
+
+export function Header(){
+  return <div className="app-header">
+    <AppLogo />
+  </div>
+}
