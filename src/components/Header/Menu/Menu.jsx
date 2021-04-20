9f174c968f328e47c5c3a1ec7c45fff8416c07ef
Laurentiu <michi.laurentiu@yahoo.com>
2021-04-20 15:19:06 +0300

    finished homework

---
+++ src/components/Header/Menu/Menu.jsx
@@ -0,0 +1,10 @@
+import { MenuButton } from "./MenuButton/MenuButton";
+import { MenuFlyout } from "./MenuFlyout/MenuFlyout";
+import "./Menu.css";
+
+export function Menu(){
+  return <div className="app-menu">
+    <MenuButton/>
+    <MenuFlyout/>
+  </div>
+}
