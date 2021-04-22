69023165dd80939ff10877ce5a78681720cd1e79
Laurentiu <michi.laurentiu@yahoo.com>
2021-04-22 18:49:30 +0300

    trash-can link not copied right

--- src/components/ToDoItem/ToDoItem.jsx
+++ src/components/ToDoItem/ToDoItem.jsx
@@ -6,6 +6,6 @@ export function ToDoItem(props){
   return <div className="to-do-item">
     <input type="checkbox" defaultChecked={props.checkValue}/>
     <p>{props.label}</p>
-    <img src="https://www.flaticon.com/svg/vstatic/svg/3221/3221897.svg?token=exp=1618938204~hmac=24b615e044a4d30fe6947517dbf56efb" alt="trash-can"/>
+    <img src="https://www.flaticon.com/svg/vstatic/svg/3221/3221897.svg?token=exp=1619107201~hmac=6781a839dc8c615ca1c540c2d51c3443" alt="trash-can"/>
   </div>
 }
