bc9af8dcb5abf37921894d3ed30021fd88aa1169
Laurentiu <michi.laurentiu@yahoo.com>
2021-04-26 12:52:50 +0300

    testing

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -6,13 +6,15 @@ import "./Form.css"
 
 export class Form extends Component{
   render(){
+    let className = "contact-form-field"
+    if(this.props.isActive) className += "contact-form-field--empty"
     return(
       <div className="contact-form">
-        <FormField label = "FIRST NAME"/>
+        <FormField label = "FIRST NAME" className={className}/>
         <FormField label = "LAST NAME"/>
         <FormField label = "EMAIL"/>
         <FormTextField label = "MESSAGE"/>
-        <SendButton/>
+        <SendButton />
       </div>
     )
   }
