beaa96f335aef259a6d10bd9ea0ad676dfb77bc1
Laurentiu <michi.laurentiu@yahoo.com>
2021-08-24 12:31:01 +0300

    move to react

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,6 +4,17 @@ import "./Header.css"
 export function Header(){
   return <div className="app-header">
     <AppLogo />
-    <p className="app-header__title">To Do List</p>
+    <nav class="navbar">
+    <div class="max-width">
+      <p class="logo">CV</p>
+      <ul class="menu">
+        <li><a href="#home" class="nav-btn">Home</a></li>
+        <li><a href="#about" class="nav-btn">About</a></li>
+        <li><a href="#skills" class="nav-btn">Skills</a></li>
+        <li><a href="#projects" class="nav-btn">Projects</a></li>
+        <li><a href="#contact" class="nav-btn">Contact</a></li>
+      </ul>
+    </div>
+  </nav>
   </div>
 }
