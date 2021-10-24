ebf84a0290f5d21c5fd6591d80c9facb61698c53
Laurentiu <michi.laurentiu@yahoo.com>
2021-10-24 15:52:30 +0300

    project tree

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Header } from "./components/Header/Header"
 import { Home } from "./components/Home/Home"
 import { About } from "./components/About/About";
 import { Skills } from "./components/Skills/Skills";
+import { Projects } from "./components/Projects/Projects";
 import './App.css';
 
 
@@ -13,6 +14,7 @@ function App() {
       <Home/>
       <About/>
       <Skills/>
+      <Projects/>
     </div>
   );
 }
---
+++ src/components/Projects/Projects.jsx
@@ -0,0 +1,17 @@
+import "./Projects.css"
+
+export function Projects(){
+  return <section className="projects">
+    <div className="projects-title" data-heading="J">
+      PROJECTS
+    </div> 
+    <div className="loading">
+      <span>Loading</span>
+    </div>
+      <div className="projects-content">
+      Projects tree under construction!
+      </div>
+   
+
+  </section>
+}
