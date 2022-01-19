f5fa07c4b2018f512e925e4267f95534e5a25d46
Laurentiu <michi.laurentiu@yahoo.com>
2022-01-19 18:15:39 +0200

    Project tree done

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,18 +1,34 @@
 import "./Projects.css"
 
+import tictac from "./Photos/tic-tac.JPG"
+import rockPaper from "./Photos/rock-paper.JPG"
+import fallingGame from "./Photos/falling-game.JPG"
 export function Projects(){
   return <section className="section-projects">
     <br id="section-projects" /><br />
     <div className="projects-title" data-heading="J">
       PROJECTS
     </div> 
-    <div className="loading">
-      <span>Loading</span>
-    </div>
-      <div className="projects-content">
-      Projects tree under construction!
+    <div id="content">
+      <div className="project" >
+        <a href="https://github.com/Laurentiu165/Projects/tree/tic-tac-toe" target="_blank">
+          <img src={tictac} alt="" />
+          <p>Tic-Tac-Toe game</p>
+        </a>
       </div>
-   
+      <div className="project">
+        <a href="https://github.com/Laurentiu165/Projects/tree/Rock-Paper-Scissors" target="_blank">
+          <img src={rockPaper} alt=""  />
+         <p>Rock Paper Scissors</p>
+        </a>
+      </div>
+      <div className="project">
+        <a href="https://github.com/Laurentiu165/Projects/tree/Falling-Ball" target="_blank">
+          <img src={fallingGame} alt="" />
+          <p>Falling ball</p>
+        </a>
+      </div>
+    </div>
 
   </section>
 }
