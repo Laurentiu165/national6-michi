0f5098cec2b0a34928d59e0280b405b51886984d
Laurentiu <michi.laurentiu@yahoo.com>
2022-01-19 23:05:05 +0200

    Project done

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,58 +1,66 @@
 import "./Contact.css"
 
+import address from "./photos/address.png"
+import phone from "./photos/phone.png"
+import email from "./photos/email.png"
+
 export function Contact(){
   return <section id="section-contact">
       <div className="contact-title" data-heading="T">
         CONTACT
       </div>
-      <div className="contact-container">
-        <div className="name-container">
-          <div className="h1-p-container">
-            <h1>Name</h1>
-            <p>Michi Laurentiu</p>
+      <div className="contentAll">
+        <div className="firstRow">
+
+          <div className="contentContact">
+            <img src={address} alt="" />
+            <p style={{color: '#ffcc00'}}>Address:</p>
+            <p>Tudor Vladimirescu Street</p>
           </div>
-        </div>
-        <div className="address-container">
-          <div className="h1-p-container">
-            <h1>Address</h1>
-            <p>Sibiu, ROMANIA</p>
+
+          <div className="contentContact">
+            <img src={phone} alt="" />
+            <p style={{color: '#ffcc00'}}>Phone:</p>
+            <p>0743041026</p>
           </div>
-        </div>
-        <div className="email-container">
-          <div className="h1-p-container">
-            <h1>Email</h1>
+
+          <div className="contentContact">
+            <img src={email} alt="" />
+            <p style={{color: '#ffcc00'}}>Email:</p>
             <p>michi.laurentiu@yahoo.com</p>
           </div>
+
         </div>
-        <div className="phone-container">
-          <div className="h1-p-container">
-            <h1>Phone</h1>
-           <p>+40 743041026</p>
+        <div className="secondRow">
+          <form action="#">
+          <div className="form">
+
+            <div className="form-group">
+              <input type="text" className="formControl" placeholder="Name" required />
+            </div>
+
+            <div className="form-group">
+              <input type="text"  className="formControl" placeholder="Email" required/>
+            </div>
+
+            <div className="form-group">
+              <input type="text"  className="formControl" placeholder="Subject" required/>
+            </div>
+
+            <div className="form-group">
+              <textarea name="" id="" cols="30" rows="8" className="textArea" className="formControl" placeholder="Message" required></textarea>
+            </div>
+            <div className="form-group">
+              {/* <input type="submit" value={"Send Message"} className="button" /> */}
+              <button type="submit" className="button">Send Message</button>
+            </div>
+
           </div>
+          </form>
         </div>
-     </div>
-     <div className="column right">
-      <form action="#">
-        <div className="fields">
-          <div className="field name">
-            <input type="text" placeholder="Name" required/>
-          </div>
-          <div className="field email">
-            <input type="email" placeholder="email" required/>
-          </div>
-        </div>
-        <div className="field">
-          <input type="text" placeholder="Subject" required/>
-        </div>
-        <div className="field textarea">
-          <textarea  cols="30" rows="10" placeholder="Describe project.." required></textarea>
-          <div className="button">
-            <button type="submit">Send message</button>
-          </div>
-        </div>
-                       
-      </form>
-    </div>
+
+      </div>
+
   </section>
    
   
